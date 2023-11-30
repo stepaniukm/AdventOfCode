6f964d496c657aca0ddfde949baa60b1b78abb3d
Mateusz Stepaniuk <stepaniuk.m05@gmail.com>
2023-11-30 13:40:26 +0100

    Init 2023

---
+++ 2023/AdventOfCode2023/AdventOfCode2023/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+
+var entry = Console.ReadLine();
+
+if (entry != null)
+{
+    Console.WriteLine(entry.ToUpper());
+}
+
