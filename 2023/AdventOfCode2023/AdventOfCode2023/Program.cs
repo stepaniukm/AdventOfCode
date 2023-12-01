e6f9efef516380e966b576c62eaf1a005885f763
Mateusz Stepaniuk <stepaniuk.m05@gmail.com>
2023-12-01 18:12:27 +0100

    feat(2023): refactor day 1

--- 2023/AdventOfCode2023/AdventOfCode2023/Program.cs
+++ 2023/AdventOfCode2023/AdventOfCode2023/Program.cs
@@ -1,12 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Reflection;
 using AdventOfCode2023.Day1;
-var d1 = new Day1();
 
 var lines = File.ReadAllText("./Day1/input.txt").Split("\n").ToList();
-var result = d1.Part1(lines);
-var result2 = d1.Part2(lines);
+var result = Day1.Part1(lines);
+var result2 = Day1.Part2(lines);
 
 Console.WriteLine(result);
 Console.WriteLine(result2);
