919d96751e2299f452997d63cf2ffbf7cdf8d07f
Mateusz Stepaniuk <stepaniuk.m05@gmail.com>
2023-12-04 22:18:06 +0100

    feat: day 4

--- 2023/AdventOfCode2023/AdventOfCode2023/Program.cs
+++ 2023/AdventOfCode2023/AdventOfCode2023/Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using AdventOfCode2023.Day3;
+using AdventOfCode2023.Day4;
 
-var lines = File.ReadAllText("./Day3/input.txt").Split("\n");
-var result = Day3.Part1(lines);
-var result2 = Day3.Part2(lines);
+var lines = File.ReadAllText("./Day4/input.txt").Split("\n");
+var result = Day4.Part1(lines);
+var result2 = Day4.Part2(lines);
 
 Console.WriteLine(result);
 Console.WriteLine(result2);
