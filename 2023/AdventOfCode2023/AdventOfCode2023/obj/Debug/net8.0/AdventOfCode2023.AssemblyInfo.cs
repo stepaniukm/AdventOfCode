3f3d20c6ef4ebcb64dda094a15b51ce8f04c9c1e
Mateusz Stepaniuk <stepaniuk.m05@gmail.com>
2024-11-30 17:03:08 +0100

    feat(2024): init with Deno setup

--- 2023/AdventOfCode2023/AdventOfCode2023/obj/Debug/net8.0/AdventOfCode2023.AssemblyInfo.cs
+++ 2023/AdventOfCode2023/AdventOfCode2023/obj/Debug/net8.0/AdventOfCode2023.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode2023")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+826e46704ae46cbb8c823874a36cbe8660122549")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eef89c8cc0cbf4f7687f03451bd80ee465b94fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode2023")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode2023")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 2024/src/day1/day1.ts
@@ -0,0 +1,6 @@
+export const part1 = (_input: string[]): number => {
+  return 0;
+};
+export const part2 = (_input: string[]): number => {
+  return 0;
+};
