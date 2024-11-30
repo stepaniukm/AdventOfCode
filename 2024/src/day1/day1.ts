769329f3e234ee3a4088056c7ad79a42b66214d0
Mateusz Stepaniuk <stepaniuk.m05@gmail.com>
2024-11-30 23:47:56 +0100

    feat(2024): add @std/data-structures

--- 2024/src/day1/day1.ts
+++ 2024/src/day1/day1.ts
@@ -1,5 +1,6 @@
 import { Range, Map, set } from "immutable";
 import { zip } from "@std/collections";
+import { RedBlackTree } from "@std/data-structures";
 
 const map = Map({ a: 1, b: 2, c: 3 });
 
@@ -11,7 +12,10 @@ export const part1 = (_input: string[]): number => {
 export const part2 = (_input: string[]): number => {
   const jsMap = set(map, "d", 10).toJS();
   const transformed = zip(Object.values(jsMap), ["jeden", "dwa", "trzy"]);
-  const newMap = Map(transformed).toSeq();
+  const newMap = Map(transformed).toSeq().toArray();
+  const tree = new RedBlackTree<[number, string]>();
 
-  return newMap.reduce((a, b) => a + b.length, 0);
+  newMap.forEach((v) => tree.insert(v));
+
+  return [...tree].reduce((a, b) => a + b[1].length, 0);
 };
