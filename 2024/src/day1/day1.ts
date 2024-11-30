f818a14ff4e4ce5c4e913f72b38c602653aa9494
Mateusz Stepaniuk <stepaniuk.m05@gmail.com>
2024-11-30 19:00:14 +0100

    feat(2024): add @std/colletions and have some fun with it

--- 2024/src/day1/day1.ts
+++ 2024/src/day1/day1.ts
@@ -1,4 +1,7 @@
-import { Range, Record } from "immutable";
+import { Range, Map, set } from "immutable";
+import { zip } from "@std/collections";
+
+const map = Map({ a: 1, b: 2, c: 3 });
 
 export const part1 = (_input: string[]): number => {
   return Range(0, 10)
@@ -6,7 +9,9 @@ export const part1 = (_input: string[]): number => {
     .reduce((a, b) => a + b);
 };
 export const part2 = (_input: string[]): number => {
-  return Record({ a: 1, b: 2, c: 3 })()
-    .toSeq()
-    .reduce((a, b) => a + b);
+  const jsMap = set(map, "d", 10).toJS();
+  const transformed = zip(Object.values(jsMap), ["jeden", "dwa", "trzy"]);
+  const newMap = Map(transformed).toSeq();
+
+  return newMap.reduce((a, b) => a + b.length, 0);
 };
