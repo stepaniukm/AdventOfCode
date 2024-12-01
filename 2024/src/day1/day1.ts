7166756481ac3f4e1866df5c76956c42afc313b3
Mateusz Stepaniuk <stepaniuk.m05@gmail.com>
2024-12-01 07:42:35 +0100

    feat(2024): day 1

--- 2024/src/day1/day1.ts
+++ 2024/src/day1/day1.ts
@@ -1,21 +1,56 @@
-import { Range, Map, set } from "immutable";
 import { zip } from "@std/collections";
-import { RedBlackTree } from "@std/data-structures";
 
-const map = Map({ a: 1, b: 2, c: 3 });
+const common = (input: string[]) => {
+  const parsedInput = input
+    .map((line) => {
+      const [num1, num2] = line.split("   ");
+      return [Number(num1), Number(num2)] as const;
+    })
+    .reduce(
+      (acc, b) => {
+        acc.a.push(b[0]);
+        acc.b.push(b[1]);
+        return acc;
+      },
+      { a: [] as number[], b: [] as number[] }
+    );
 
-export const part1 = (_input: string[]): number => {
-  return Range(0, 10)
-    .toSeq()
-    .reduce((a, b) => a + b);
+  return parsedInput;
 };
-export const part2 = (_input: string[]): number => {
-  const jsMap = set(map, "d", 10).toJS();
-  const transformed = zip(Object.values(jsMap), ["jeden", "dwa", "trzy"]);
-  const newMap = Map(transformed).toSeq().toArray();
-  const tree = new RedBlackTree<[number, string]>();
 
-  newMap.forEach((v) => tree.insert(v));
+export const part1 = (input: string[]): number => {
+  const parsedInput = common(input);
+  const sortedA = parsedInput.a.toSorted();
+  const sortedB = parsedInput.b.toSorted();
+  const zipped = zip(sortedA, sortedB);
+  const mapped = zipped.map(([a, b]) => Math.abs(a - b));
+  const sum = mapped.reduce((acc, b) => acc + b, 0);
 
-  return [...tree].reduce((a, b) => a + b[1].length, 0);
+  return sum;
+};
+export const part2 = (input: string[]): number => {
+  const parsedInput = common(input);
+
+  const occurancesByNumber = parsedInput.b.reduce((acc, curr) => {
+    if (curr in acc) {
+      acc[curr]++;
+    } else {
+      acc[curr] = 1;
+    }
+    return acc;
+  }, {} as Record<number, number>);
+
+  const result = parsedInput.a
+    .map((num) => {
+      const occurancesOfNum = occurancesByNumber[num];
+      console.log({ occurancesOfNum });
+      if (!occurancesOfNum) {
+        return 0;
+      }
+
+      return occurancesOfNum * num;
+    })
+    .reduce((acc, curr) => acc + curr, 0);
+
+  return result;
 };
