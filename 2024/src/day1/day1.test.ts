2abd80db0250c698dfb138cc6ccc9c79d60a922e
Mateusz Stepaniuk <stepaniuk.m05@gmail.com>
2024-12-02 15:13:50 +0100

    feat(2024): use familiar bdd syntax in tests

--- 2024/src/day1/day1.test.ts
+++ 2024/src/day1/day1.test.ts
@@ -1,44 +1,45 @@
 import { part1, part2 } from "./day1.ts";
+import { describe, it } from "@std/testing/bdd";
 import { assertEquals } from "@std/assert";
 import { join } from "@std/path";
 import { getLines } from "#utils/string.ts";
 
-Deno.test("part1 - simple input", async () => {
-  const simplePath = join(import.meta.dirname!, "simple-input.txt");
-  const inputText = await Deno.readTextFile(simplePath);
-  const lines = getLines(inputText);
+describe("Day1", () => {
+  it("should work for part 1 with simple input", async () => {
+    const simplePath = join(import.meta.dirname!, "simple-input.txt");
+    const inputText = await Deno.readTextFile(simplePath);
+    const lines = getLines(inputText);
 
-  const result = part1(lines);
+    const result = part1(lines);
 
-  assertEquals(result, 11);
-});
+    assertEquals(result, 11);
+  });
 
-Deno.test("part1 - input", async () => {
-  const simplePath = join(import.meta.dirname!, "input.txt");
-  const inputText = await Deno.readTextFile(simplePath);
-  const lines = getLines(inputText);
+  it("should work for part 1 with real input", async () => {
+    const simplePath = join(import.meta.dirname!, "input.txt");
+    const inputText = await Deno.readTextFile(simplePath);
+    const lines = getLines(inputText);
 
-  const result = part1(lines);
+    const result = part1(lines);
 
-  assertEquals(result, 1189304);
-});
+    assertEquals(result, 1189304);
+  });
+  it("should work for part 2 with simple input", async () => {
+    const simplePath = join(import.meta.dirname!, "simple-input.txt");
+    const inputText = await Deno.readTextFile(simplePath);
+    const lines = getLines(inputText);
 
-Deno.test("part2 - simple input", async () => {
-  const simplePath = join(import.meta.dirname!, "simple-input.txt");
-  const inputText = await Deno.readTextFile(simplePath);
-  const lines = getLines(inputText);
+    const result = part2(lines);
 
-  const result = part2(lines);
+    assertEquals(result, 31);
+  });
+  it("should work for part 2 with real input", async () => {
+    const simplePath = join(import.meta.dirname!, "input.txt");
+    const inputText = await Deno.readTextFile(simplePath);
+    const lines = getLines(inputText);
 
-  assertEquals(result, 31);
-});
+    const result = part2(lines);
 
-Deno.test("part2 - input", async () => {
-  const simplePath = join(import.meta.dirname!, "input.txt");
-  const inputText = await Deno.readTextFile(simplePath);
-  const lines = getLines(inputText);
-
-  const result = part2(lines);
-
-  assertEquals(result, 24349736);
+    assertEquals(result, 24349736);
+  });
 });
--- 2024/src/day2/day2.test.ts
+++ 2024/src/day2/day2.test.ts
@@ -2,43 +2,44 @@ import { part1, part2 } from "./day2.ts";
 import { assertEquals } from "@std/assert";
 import { join } from "@std/path";
 import { getLines } from "#utils/string.ts";
+import { describe, it } from "@std/testing/bdd";
 
-Deno.test("part1 - simple input", async () => {
-  const simplePath = join(import.meta.dirname!, "simple-input.txt");
-  const inputText = await Deno.readTextFile(simplePath);
-  const lines = getLines(inputText);
+describe("Day2", () => {
+  it("should work for part 1 with simple input", async () => {
+    const simplePath = join(import.meta.dirname!, "simple-input.txt");
+    const inputText = await Deno.readTextFile(simplePath);
+    const lines = getLines(inputText);
 
-  const result = part1(lines);
+    const result = part1(lines);
 
-  assertEquals(result, 2);
-});
+    assertEquals(result, 2);
+  });
 
-Deno.test("part1 - input", async () => {
-  const simplePath = join(import.meta.dirname!, "input.txt");
-  const inputText = await Deno.readTextFile(simplePath);
-  const lines = getLines(inputText);
+  it("should work for part 1 with real input", async () => {
+    const simplePath = join(import.meta.dirname!, "input.txt");
+    const inputText = await Deno.readTextFile(simplePath);
+    const lines = getLines(inputText);
 
-  const result = part1(lines);
+    const result = part1(lines);
 
-  assertEquals(result, 230);
-});
+    assertEquals(result, 230);
+  });
+  it("should work for part 2 with simple input", async () => {
+    const simplePath = join(import.meta.dirname!, "simple-input.txt");
+    const inputText = await Deno.readTextFile(simplePath);
+    const lines = getLines(inputText);
 
-Deno.test("part2 - simple input", async () => {
-  const simplePath = join(import.meta.dirname!, "simple-input.txt");
-  const inputText = await Deno.readTextFile(simplePath);
-  const lines = getLines(inputText);
+    const result = part2(lines);
 
-  const result = part2(lines);
+    assertEquals(result, 4);
+  });
+  it("should work for part 2 with real input", async () => {
+    const simplePath = join(import.meta.dirname!, "input.txt");
+    const inputText = await Deno.readTextFile(simplePath);
+    const lines = getLines(inputText);
 
-  assertEquals(result, 4);
-});
+    const result = part2(lines);
 
-Deno.test("part2 - input", async () => {
-  const simplePath = join(import.meta.dirname!, "input.txt");
-  const inputText = await Deno.readTextFile(simplePath);
-  const lines = getLines(inputText);
-
-  const result = part2(lines);
-
-  assertEquals(result, 301);
+    assertEquals(result, 301);
+  });
 });
